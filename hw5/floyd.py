0c1feb9543c1ceccf889a5a04e00ff5176ee758f
Roy Hershfinkel <hersh.roy@gmail.com>
2024-08-03 12:31:41 +0300

    Added implementation of floyd.py

--- hw5/floyd.py
+++ hw5/floyd.py
@@ -11,7 +11,31 @@ def find_collision(f, start):
     :param start: starting point
     :return: x_0, x_1 such that x_0 != x_1 and f(x_0) = f(x_1)
     """
-    ?
+    # Maybe we are lucky
+    if f.calc(start) == start:
+        return start, start
+
+    p1 = f.calc(start)
+    p2 = f.calc(f.calc(start))
+    count = 0
+    while p1 != p2:
+        count += 1
+        p1 = f.calc(p1)
+        p2 = f.calc(f.calc(p2))
+
+    # Check if we started at the cycle
+    for _ in range(count * 2):
+        p1 = f.calc(p1)
+        if p1 == start:
+            print("Failed")
+            return start, start
+
+    # Find the collision point
+    p1 = start
+    while f.calc(p1) != f.calc(p2):
+        p1 = f.calc(p1)
+        p2 = f.calc(p2)
+    return p1, p2
 
 
 def main():
@@ -28,7 +52,8 @@ def main():
         else:
             print("Fail")
             # What needs to be modified here so that the attack eventually succeeds?
-            ?
+            # We just need to start at a different point, any different point will do
+            start += 1
 
 
 if __name__ == "__main__":
